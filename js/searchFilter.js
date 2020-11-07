c02cd229045a16fdcb7aac6431f2b5d6a99e07f8
Bee Kay <brittkcrouch@gmail.com>
2020-11-07 13:40:10 -0600

    Search Filter Troubleshoot

--- js/searchFilter.js
+++ js/searchFilter.js
@@ -1,18 +1,23 @@
+
 function filter() {
     
-    var filterValue, input, ul, li, a, i;
+    var input, ul, li, a, i;
         input = document.getElementById("search");
-        filterValue = input.value.toUpperCase();
-        ul = document.getElementById("data-caption");
+        addEventListener("keyup");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("gallery");
         li = ul.getElementsByTagName("li");
         
         for (i = 0 ; i < li.length ; i++){
             a = li[i].getElementsByTagName("a")[0];
+
             if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
                 li[i].style.display = "";
                 
-            }else{
+            } else {
                 li[i].style.display = "none";
             }
         }
     }
+
+    console.log(search);
